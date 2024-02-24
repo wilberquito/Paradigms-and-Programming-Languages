595db88b947b0a90dfa70d76e0f8d6b861ca9b4c
jordicollcaballero <collc2@gmail.com>
2024-02-24 16:25:16 +0100

    Update cliques.pl

--- Prolog/Solucions/Sessio1/cliques.pl
+++ Prolog/Solucions/Sessio1/cliques.pl
@@ -1,5 +1,5 @@
 
-%Les arestes no son dirigides, pero s'especifiquen repeticions i ordenades lexicograficament
+%Les arestes no son dirigides, pero s'especifiquen sense repeticions i ordenades lexicograficament
 %aresta(X,Y) => hi ha una aresta entre X i Y, i X precedeix lexicograficament Y
 %per exemple, es pot demostrar aresta(n1,n2), pero no aresta(n2,n1)
 aresta(n1,n2).
@@ -23,5 +23,5 @@ clique3(A,B,C) :- aresta(A,B), aresta(A,C), aresta(B,C).
 %clique4(A,B,C,D) => El conjunt {A,B,C,D} es un clique, i la sequencia A,B,C,D esta ordenada lexicograficament
 clique4(A,B,C,D) :- clique3(A,B,C), aresta(A,D), aresta(B,D), aresta(C,D).
 
-%Alternativa menys eficient. Tot i que es correcta, la opcio anterior es millor, perque demostrar aresta es molt mes facil que demostrar clique3.
+%Alternativa menys eficient. Tot i que aquesta definicio es correcta, l'opcio anterior es millor, perque demostrar aresta es molt mes facil que demostrar clique3.
 %clique4(A,B,C,D) :- clique3(A,B,C), clique3(A,B,D), clique3(A,C,D), clique3(B,C,D).
