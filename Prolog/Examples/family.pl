2fce95f558f907fd1cf381fdbc5dbc75472eb16b
wilberquito <typingwil@gmail.com>
2026-02-16 18:02:14 +0100

    Adds family prolog example

---
+++ Prolog/Examples/family.pl
@@ -0,0 +1,9 @@
+father(tom,jack). %f1
+father(tom,lisa). %f2
+father(wil,tom).  %f3
+mother(ann,tom).  %f4
+mother(ann,john). %f5
+parent(X1,Y1):-father(X1,Y1). %r1
+parent(X2,Y2):-mother(X2,Y2). %r2
+grandfather(X3,Y3):-father(X3,Z3),parent(Z3,Y3).     %r3
+siblings(X4,Y4):-parent(Z4,X4),parent(Z4,Y4),X4\=Y4. %r4
